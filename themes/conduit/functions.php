e3120fda0b7ce8311f81b1f5ba4b02f88d5e13c3
Garrett <gvorbeck@gmail.com>
2014-10-29 21:38:34 -0400

    more

--- themes/conduit/functions.php
+++ themes/conduit/functions.php
@@ -6,6 +6,15 @@ add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
 add_theme_support( 'post-thumbnails' );
 // Add theme support for Automatic Feeds Links
 add_theme_support( 'automatic-feed-links' );
+// Add theme support for HTML5
+$args = array(
+  'search-form',
+  'comment-form',
+  'comment-list',
+  'gallery',
+  'caption'
+);
+add_theme_support( 'html5', $args );
 
 // Define SITE_URL global variable for dev purposes.
 define('SITE_URL', $_SERVER['HTTP_HOST']);
@@ -49,7 +58,7 @@ if ( ! is_admin() && ! is_login_page() ) {
 // Replaces the excerpt "more" text by a link
 function conduit_excerpt_more($more) {
   global $post;
-	return ' <a class="post--more" href="'. get_permalink($post->ID) . '">Read more...</a>';
+  return ' <a class="post--more" href="'. get_permalink($post->ID) . '">Read more...</a>';
 }
 add_filter('excerpt_more', 'conduit_excerpt_more');
 
@@ -74,4 +83,7 @@ function conduit_comment_list( $id ) {
 }
 function conduit_svg_hamburger() {
   return '<svg class="svg svg--hamburger" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3 18h18v-2H3V18z M3 13h18v-2H3V13z M3 6v2h18V6H3z"/></svg>';
+}
+function conduit_svg_gear() {
+  return '<svg class="svg svg--admin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="21.59px" height="21.137px" viewBox="0 0 21.59 21.137" xml:space="preserve"><path d="M18.622 8.145L18.077 6.85c0 0 1.268-2.861 1.156-2.971L17.554 2.2 c-0.116-0.113-2.978 1.193-2.978 1.193L13.256 2.9c0 0-1.166-2.9-1.326-2.9H9.561C9.396 0 8.3 2.9 8.3 2.906L6.999 3.4 c0 0-2.922-1.242-3.034-1.131L2.289 3.951C2.173 4.1 3.5 6.9 3.5 6.867L2.962 8.16C2.962 8.2 0 9.3 0 9.455v2.322 c0 0.2 3 1.2 3 1.219l0.545 1.291c0 0-1.268 2.859-1.157 2.969l1.678 1.643c0.114 0.1 2.977-1.195 2.977-1.195 l1.321 0.535c0 0 1.2 2.9 1.3 2.898h2.369c0.164 0 1.244-2.906 1.244-2.906l1.322-0.535c0 0 2.9 1.2 3 1.1 l1.678-1.641c0.117-0.115-1.22-2.916-1.22-2.916l0.544-1.293c0 0 2.963-1.143 2.963-1.299v-2.32 C21.59 9.2 18.6 8.1 18.6 8.145z M14.256 10.568c0 1.867-1.553 3.387-3.461 3.387c-1.906 0-3.461-1.52-3.461-3.387 s1.555-3.385 3.461-3.385C12.704 7.2 14.3 8.7 14.3 10.568z" class="style1"/></svg>';
 }
--- themes/conduit/header.php
+++ themes/conduit/header.php
@@ -1,52 +1,44 @@
 <?php ?>
 <!DOCTYPE html>
-<!--[if IE 9 ]><html lang="en" class="ie ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
 <head>
-<title><?php
-	global $page, $paged;
-	wp_title( '|', true, 'right' );
-		bloginfo( 'name' );
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) )
-			echo " | $site_description";
-		if ( $paged >= 2 || $page >= 2 )
-			echo ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
-	?>
-</title>
-<meta name="description" content="<?php if ( is_single() ) {
-	single_post_title('', true);
-	} else {
-	bloginfo('name'); echo " - "; bloginfo('description');
-	}
-?>" />
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<!-- The little things -->
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-  <link rel="shortcut icon" href="<?php echo bloginfo('template_directory'); ?>/favicon.png">
-<!-- The little things -->
-
-	<?php wp_head(); ?>
-
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?php wp_title( '|', true, 'right' ); ?></title>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  <?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
-    <header role="banner">
-        <?php echo conduit_svg_hamburger(); ?>
-        <h1><a id="site--title" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo"><?php bloginfo( 'name' ); ?></a></h1>
-    </header>
-    <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'menu-hamburger-container' ) ); ?>
-    <a href="<?php echo admin_url() . 'post-new.php'; ?>" class="button--add-post button">
+  <header role="banner">
+    <?php echo conduit_svg_hamburger(); ?>
+    <h1>
+      <a id="site--title" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home" class="logo">
+        <?php bloginfo( 'name' ); ?>
+      </a>
+    </h1>
+    <?php get_search_form(); ?>
+    <a id="site--admin" href="<?php echo esc_attr( admin_url() ); ?>"><?php echo conduit_svg_gear(); ?></a>
+  </header>
+  <?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'menu-hamburger-container' ) ); ?>
+  <?php if ( is_user_logged_in() && current_user_can( 'edit_posts' ) ) { ?>
+    <a href="<?php echo esc_attr( admin_url() ) . 'post-new.php'; ?>" class="button--add-post button">
       <span>+</span>
     </a>
-    <main>
-      <?php if ( is_page() ) {
-        echo '<h1>' . get_the_title() . '</h1>';
-      } else if ( is_category() ) {
-        echo '<h1>';
-        single_cat_title();
-        echo '</h1>';
-      }
+  <?php } ?>
+  <main>
+    <?php if ( is_page() ) {
+      echo '<h1>' . get_the_title() . '</h1>';
+    } else if ( is_category() ) {
+      echo '<h1>';
+      single_cat_title();
+      echo '</h1>';
+    }
