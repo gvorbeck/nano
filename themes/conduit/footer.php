da5726d5519f679cf27a50e42dad4bb132f2016e
gvorbeck <gvorbeck@gmail.com>
2014-10-30 15:02:17 -0400

    footer stuff

--- themes/conduit/footer.php
+++ themes/conduit/footer.php
@@ -1,5 +1,8 @@
-</main>
-<footer></footer>
-<?php wp_footer(); ?>
-</body>
+    </main>
+    <footer id="site-footer">
+      <p id="copyright">&copy; <?php echo date('Y') . ' ' . get_bloginfo('name'); ?></p>
+      <p id="site-credit">Created by <a href="http://iamgarrett.com" title="iamgarrett">Garrett Vorbeck</a>.</p>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
 </html>
